b66d17b9428fd17f088e913b9d4efe5cb25afdef
Hmiya6 <h.miya.work@gmail.com>
2021-04-27 17:28:30 +0900

    small change

--- src/insertion.rs
+++ src/insertion.rs
@@ -1,7 +1,7 @@
 use std::fmt::{Display, Debug};
 
 // INSERTION SORT
-//
+// Ave: O(n^2), Best: O(n), Worst: O(n^2)
 //
 // Example: 1 7 3 2 8 5
 // 1 (i) 7 2 3 8 5, i = index
@@ -11,7 +11,7 @@ use std::fmt::{Display, Debug};
 // 1 2 3 (i) 7 8 5 <- swapped 2, 3
 // 1 2 3 7 (i) 8 5
 // 1 2 3 7 8 (i) 5
-// 1 2 3 5 7 (i) 8 <- swapped 3, 5 (<- compare tmp, 4 <- compare tmp, 5), tmp = 3
+// 1 2 3 5 7 (i) 8 <- swapped 3, 5 (<- compare 3, 4 <- compare 4, 5)
 pub fn insertion_sort<T>(src: &mut [T])
     where T: PartialOrd + Copy + Display + Debug
 {
