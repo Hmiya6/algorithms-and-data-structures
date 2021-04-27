05ef91ee80cdbd01168f47bbca6a5e3d6e36a719
Hmiya6 <h.miya.work@gmail.com>
2021-04-27 19:30:49 +0900

    Add comment

--- src/shell.rs
+++ src/shell.rs
@@ -1,6 +1,19 @@
 use std::fmt::{Display, Debug};
 
-
+// SHELL SORT
+// Best: O(n*log(n)), Worst: O(n^2)
+//
+// Example: 5 6 9 2 3
+// 5 6 9 2 3 -> gap = len/2 /* == 2 */
+// 5 6 9 2 3 <- compared 0, 2 (no swap)
+// 5 2 9 6 3 <- swapped 1, 3 (<- compared 1, 3)
+// 5 2 3 6 9 <- swapped 2, 4 (<- compared 2, 4)
+// 3 2 5 6 9 <- swapped 0, 2
+//  -> gap /= 2 /* == 1 */
+// 2 3 5 6 9 <- swapped 0, 1
+// 2 3 5 6 9 <- compared 1, 2
+// 2 3 5 6 9 <- compared 2, 3
+// 2 3 5 6 9 <- compared 3, 4
 pub fn shell_sort<T>(src: &mut [T])
     where T: PartialOrd + Copy + Display + Debug
 {
